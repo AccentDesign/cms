5d05506954e52ba6518714d69228ddab6b45289e
Stuart George <stuart@accentdesign.co.uk>
2024-12-02 16:45:51 +0000

    use safelist for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./pkg/ui/**/*{.templ,.html}"],
+    content: ["./pkg/ui/**/*.templ"],
     theme: {
         extend: {},
     },
+    safelist: [
+        "bg-gray-200",
+        "flex",
+        "h-32",
+        "h-4",
+        "h-48",
+        "items-center",
+        "rounded",
+        "rounded-full",
+        "rounded-lg",
+        "space-x-4",
+        "space-y-2",
+        "space-y-3",
+        "space-y-6",
+        "w-1/2",
+        "w-1/3",
+        "w-2/3",
+        "w-32",
+        "w-4/6",
+        "w-5/6",
+        "w-full"
+    ],
     plugins: [],
 }
