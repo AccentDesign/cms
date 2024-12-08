ad2f94983171ea77a7b67e0b79cdb94db18b0337
Stuart George <stuart@accentdesign.co.uk>
2024-12-08 19:16:14 +0000

    use MustSubFS in static

--- pkg/static/router.go
+++ pkg/static/router.go
@@ -3,8 +3,6 @@ package static
 import (
 	"embed"
 	"github.com/labstack/echo/v4"
-	"io/fs"
-	"log"
 )
 
 var (
@@ -14,14 +12,5 @@ var (
 
 // Router create a new static router.
 func Router(e *echo.Echo) {
-	e.StaticFS("/static", staticFS())
-}
-
-// staticFS returns the static file system.
-func staticFS() fs.FS {
-	s, err := fs.Sub(Public, "public")
-	if err != nil {
-		log.Fatalf("Unable to load static files: %v", err)
-	}
-	return s
+	e.StaticFS("/static", echo.MustSubFS(Public, "public"))
 }
