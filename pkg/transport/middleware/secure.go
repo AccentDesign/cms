eeadabbf09f0a8ae6441a7c2f665be263d3b8770
Stuart George <stuart@accentdesign.co.uk>
2024-12-13 16:22:40 +0000

    add nonce and allow inline scripts

--- pkg/transport/middleware/secure.go
+++ pkg/transport/middleware/secure.go
@@ -1,24 +1,49 @@
 package middleware
 
 import (
+	"crypto/rand"
 	"echo.go.dev/pkg/config"
+	"encoding/base64"
+	"github.com/a-h/templ"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"strings"
 )
 
-// Secure returns an Echo middleware function for cors.
+// Secure returns a Secure middleware with config.
 func Secure(cfg config.SecurityConfig) echo.MiddlewareFunc {
-	secureConfig := middleware.SecureConfig{
-		XSSProtection:         cfg.XSSProtection,
-		ContentTypeNosniff:    cfg.ContentTypeNosniff,
-		XFrameOptions:         cfg.XFrameOptions,
-		HSTSMaxAge:            cfg.HSTSMaxAge,
-		ContentSecurityPolicy: cfg.ContentSecurityPolicy,
-		ReferrerPolicy:        cfg.ReferrerPolicy,
-		Skipper: func(c echo.Context) bool {
-			return c.Path() != "/*"
-		},
-	}
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			nonce := generateNonce()
 
-	return middleware.SecureWithConfig(secureConfig)
+			cspWithNonce := strings.ReplaceAll(cfg.ContentSecurityPolicy, "nonce-", "nonce-"+nonce)
+
+			secureConfig := middleware.SecureConfig{
+				XSSProtection:         cfg.XSSProtection,
+				ContentTypeNosniff:    cfg.ContentTypeNosniff,
+				XFrameOptions:         cfg.XFrameOptions,
+				HSTSMaxAge:            cfg.HSTSMaxAge,
+				ContentSecurityPolicy: cspWithNonce,
+				ReferrerPolicy:        cfg.ReferrerPolicy,
+				Skipper: func(c echo.Context) bool {
+					return c.Path() != "/*"
+				},
+			}
+
+			ctx := templ.WithNonce(c.Request().Context(), nonce)
+			c.SetRequest(c.Request().WithContext(ctx))
+
+			return middleware.SecureWithConfig(secureConfig)(next)(c)
+		}
+	}
+}
+
+// generateNonce generates a random base64 nonce.
+func generateNonce() string {
+	nonce := make([]byte, 16) // 16 bytes = 128 bits
+	_, err := rand.Read(nonce)
+	if err != nil {
+		panic("failed to generate nonce: " + err.Error())
+	}
+	return base64.StdEncoding.EncodeToString(nonce)
 }
